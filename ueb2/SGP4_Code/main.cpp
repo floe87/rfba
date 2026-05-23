a4fa1d482d244991207611d98b66e3ab116eda04
flo <flo.ruehlmann@outlook.de>
2026-05-23 13:58:45 +0200

    ueb2 setup

---
+++ ueb2/SGP4_Code/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+#include <map>
+#include "omm/omm.h"
+#include "omm/ommreader.h"
+#include "sgp4/timeDate.h"
+#include "sgp4/coordinates.h"
+#include "sgp4/SGP4Propagator.h"
+
+
+int main(int argc, char *argv[])
+{
+	// File with TLEs
+	string fileName = "omm.txt";
+	//cout << "Test" << endl;
+	// Get Map from txt file
+	map<int, OMM> satMap = readOmmFile(fileName.c_str());
+	//OMM omm = satMap[12345];	// Beispiel
+
+	OMM sonate = satMap[59112];
+	sonate.print();
+	return 0;
+}
